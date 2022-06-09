d5d1a70a27705a6e3f0457626fa9a9dd70a5f321
PaDuckk <kang10019@gmail.com>
2022-06-09 23:52:14 +0900

    hello cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-c0f399812e7ebee4.d
@@ -0,0 +1,5 @@
+/Users/deokyoung/workspace/paduckk/hello-rust/hello_cargo/target/debug/deps/hello_cargo-c0f399812e7ebee4: src/main.rs
+
+/Users/deokyoung/workspace/paduckk/hello-rust/hello_cargo/target/debug/deps/hello_cargo-c0f399812e7ebee4.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/deokyoung/workspace/paduckk/hello-rust/hello_cargo/target/debug/hello_cargo: /Users/deokyoung/workspace/paduckk/hello-rust/hello_cargo/src/main.rs
